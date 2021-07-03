718409c2e41b57ec70866d38a9ea9c5de870b8d2
OderoOluoch <oderoluoch@gmail.com>
2021-07-03 22:31:55 +0300

    All screens in place, now functionality and polish

--- app/src/main/java/com/odero/bigbeats/LogInActivity.java
+++ app/src/main/java/com/odero/bigbeats/LogInActivity.java
@@ -2,13 +2,25 @@ package com.odero.bigbeats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
-
+    Button seeMusic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        seeMusic = findViewById(R.id.goToSeeMusicFromLogIn);
+        seeMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInActivity.this, MusicActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/odero/bigbeats/RegisterActivity.java
+++ app/src/main/java/com/odero/bigbeats/RegisterActivity.java
@@ -2,13 +2,27 @@ package com.odero.bigbeats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button seeMusic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        seeMusic = findViewById(R.id.goToSeeMusciFRomRegister);
+        seeMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MusicActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
