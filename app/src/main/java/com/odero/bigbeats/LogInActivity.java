c99dd9d10917a365b94a85d628b9b808d29717c2
OderoOluoch <oderoluoch@gmail.com>
2021-07-04 00:15:39 +0300

    Set username from login view to music list view

--- app/src/main/java/com/odero/bigbeats/LogInActivity.java
+++ app/src/main/java/com/odero/bigbeats/LogInActivity.java
@@ -6,19 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
-    Button seeMusic;
+    private Button seeMusic;
+    private EditText mUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
+        mUserName = findViewById(R.id.nameInputLoginView);
         seeMusic = findViewById(R.id.goToSeeMusicFromLogIn);
         seeMusic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String userName = mUserName.getText().toString();
                 Intent intent = new Intent(LogInActivity.this, MusicActivity.class);
+                intent.putExtra("userName", userName);
                 startActivity(intent);
             }
         });
