565b6f4318f80a642929b9e8e9129dc1fab6e742
OderoOluoch <oderoluoch@gmail.com>
2021-07-03 14:17:37 +0300

    Splash screen and animations in place

--- app/src/main/java/com/odero/bigbeats/MainActivity.java
+++ app/src/main/java/com/odero/bigbeats/MainActivity.java
@@ -1,12 +1,22 @@
  package com.odero.bigbeats;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
 
  public class MainActivity extends AppCompatActivity {
+    //Hooks variables.
+     View first,second,third,fourth,fifth,sixth;
+     TextView tagLine,logoText;
+
+
+     //Animation variables
+     Animation topAnimation,bottomAnimation,middleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +24,29 @@ import android.view.WindowManager;
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this,R.anim.middle_animation);
+
+        //Hooks
+        first = findViewById(R.id.first_line);
+        second = findViewById(R.id.second_line);
+        third = findViewById(R.id.third_line);
+        fourth = findViewById(R.id.fourth_line);
+        fifth = findViewById(R.id.fifth_line);
+        sixth = findViewById(R.id.sixth_line);
+
+        tagLine = findViewById(R.id.tagLine);
+        logoText = findViewById(R.id.textLogo);
+
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+
+        logoText.setAnimation(middleAnimation);
+        tagLine.setAnimation(bottomAnimation);
     }
 }
