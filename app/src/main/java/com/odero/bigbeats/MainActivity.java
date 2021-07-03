2fefd2cf04767e670d49635c5caec7b67808f947
OderoOluoch <oderoluoch@gmail.com>
2021-07-03 13:28:21 +0300

    Views and dawables for the splash screen in place

--- app/src/main/java/com/odero/bigbeats/MainActivity.java
+++ app/src/main/java/com/odero/bigbeats/MainActivity.java
@@ -2,6 +2,7 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/academykiller.ttf");
     }
 }
