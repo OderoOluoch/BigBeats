3aa2cc744e74433d030d0c59d61393892bba7f3c
OderoOluoch <oderoluoch@gmail.com>
2021-07-03 13:41:20 +0300

    Layout tested and working well.Now starting to animate

--- app/src/main/java/com/odero/bigbeats/MainActivity.java
+++ app/src/main/java/com/odero/bigbeats/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/academykiller.ttf");
+
     }
 }
