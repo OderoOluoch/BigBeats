96fafbb10427eacfb092728d975dff1b5415d56b
OderoOluoch <oderoluoch@gmail.com>
2021-07-04 16:34:00 +0300

    register activity refactored

--- app/src/main/java/com/odero/bigbeats/RegisterActivity.java
+++ app/src/main/java/com/odero/bigbeats/RegisterActivity.java
@@ -8,16 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import butterknife.BindView;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    private Button seeMusic;
-    private EditText mRegisterUserName;
+    //Using ButterKnife to bind views
+    @BindView(R.id.goToSeeMusciFRomRegister) Button seeMusic;
+    @BindView(R.id.nameInputRegisterView) EditText mRegisterUserName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        mRegisterUserName = findViewById(R.id.nameInputRegisterView);
-        seeMusic = findViewById(R.id.goToSeeMusciFRomRegister);
         seeMusic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
